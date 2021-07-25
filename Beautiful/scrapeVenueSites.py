8d578a16a874bf85ee03f473018a3981a588a256
Connor-Jennings <jennings.co.d@gmail.com>
2021-07-24 18:24:26 -0700

    made driver global

--- Beautiful/scrapeVenueSites.py
+++ Beautiful/scrapeVenueSites.py
@@ -68,7 +68,8 @@ def format_date(u_date,site):
         "September" : "09",
         "October" : "10",
         "November" : "11",
-        "December" : "12"
+        "December" : "12",
+        "Sept" : "09"
     }
     if u_date == "None":
         return u_date
@@ -150,24 +151,34 @@ def format_date(u_date,site):
         date += year[2] + year[3]
         return date
 
+    elif site == "hollywood":
+        split = re.split("-", u_date)
+        date = split[1] + "/" + split[2] +"/"
+        year = split[0]
+        date += year[2] + year[3]
+        return date
+    
+    elif site == "hondacenter":
+        year = str(datetime.date.today().year)
+        split = u_date.replace(".","")
+        split = re.split(" ", split)
+        date = months[split[1]] + "/"
+        if int(split[2]) < 10:
+            split[2] = "0"+split[2]
+        date += split[2] + "/" + year[2] + year[3]
+        return date
+
+    return u_date
 #####################################################################################################################################
 def find_year(element):
     if hasattr(element.div.previous_sibling, "tribe-events-calendar-list__month-separator"):
         return str(element.text).strip()
     else:
         find_year(element.previous_sibling)
-        
+
 
 #####################################################################################################################################
-def templateFunction(data):
-    # initiating the web driver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+def templateFunction(driver, data):
     driver.get('https://website.com') 
 
     # get the source html 
@@ -199,8 +210,6 @@ def templateFunction(data):
         data.append("Venue Website", "venue name", headliner_array[i], date_array[i], link_array[i])
         i+=1
 
-    # close the connection 
-    driver.close() 
     return
    
 
@@ -208,15 +217,7 @@ def templateFunction(data):
 #####################################################################################################################################
 #                                                   Venue Functions                                                                 #
 # Scrape Santa Barbara Bowl #########################################################################################################
-def SB(data):
-     # initiating the web driver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+def SB(driver, data):
     driver.get('https://sbbowl.com/concerts') 
 
     # get the source html 
@@ -255,21 +256,11 @@ def SB(data):
         data.append("Venue Website", "Santa Barbara Bowl", headliner_array[i], date_array[i], link_array[i]) 
         i+=1
 
-    # close the connection 
-    driver.close() 
     return 
 
 
 # Scrape The Fonda Theatre ##########################################################################################################
-def fonda(data):
-    # initiating the web driver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+def fonda(driver, data):
     driver.get('https://www.fondatheatre.com/events/all') 
 
     # get the source html 
@@ -313,20 +304,10 @@ def fonda(data):
         data.append("Venue Website", "The Fonda Theatre", headliner_array[i], date_array[i],link_array[i])
         i+=1
 
-    # close the connection 
-    driver.close() 
     return
 
 # Scrape The Forum Inglewood ########################################################################################################
-def forum(data): 
-    # initiating the web driver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+def forum(driver, data): 
     driver.get('https://thelaforum.com/events/list/') 
 
     # get the source html 
@@ -360,20 +341,10 @@ def forum(data):
         data.append("Venue Website", "The Forum Inglewood",headliner_array[i], date_array[i], link_array[i])
         i+=1
 
-    # close the connection 
-    driver.close() 
     return
 
 # Scrape Greek Theatre Los Angeles ##################################################################################################
-def greekLA(data):
-    # initiating the web driver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+def greekLA(driver, data):
     driver.get('https://www.lagreektheatre.com/events/all') 
 
     # get the source html 
@@ -406,21 +377,11 @@ def greekLA(data):
         data.append("Venue Website", "Greek Theatre Los Angeles", headliner_array[i], date_array[i], link_array[i])
         i+=1
 
-    # close the connection 
-    driver.close() 
     return
    
 
 # Scrape Greek Theatre-U.C. Berkeley ################################################################################################
-def greekBerkley(data):
-    # initiating the web driver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+def greekBerkley(driver, data):
     driver.get('https://thegreekberkeley.com/event-listing/') 
 
     # get the source html 
@@ -458,22 +419,30 @@ def greekBerkley(data):
         data.append("Venue Website", "Greek Theatre-U.C. Berkeley", headliner_array[i], date_array[i], link_array[i])
         i+=1
 
-    # close the connection 
-    driver.close() 
     return
     
 
 # Scrape Hollywood Bowl Hollywood ###################################################################################################
-def hollywood(data):
-    # initiating the web driver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+def hollywood(driver, data):
     driver.get('https://www.hollywoodbowl.com/events/performances?Venue=Hollywood+Bowl&Season=null') 
+
+    # scroll down to load whole page
+
+    # Get scroll height.
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to the bottom.
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load the page.
+        time.sleep(3)
+
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+        last_height = new_height
 
     # get the source html 
     html = driver.page_source
@@ -490,10 +459,18 @@ def hollywood(data):
     headliners = soup.find_all('span', class_="name name--short")
 
     for headliner in headliners:
-        headliner_array.append(str(headliner.text).strip())
-        # link_array.append(str(headliner['href']).strip())
+        head = strip(str(headliner.text).strip())
+        while "  " in head:
+            head = re.sub("  ", " ", head)
+        headliner_array.append(head)
 
     # select date and add to array 
+    dates = soup.find_all('div', class_="performance-card__anchor")
+    for date in dates:
+        day = date['data-day']
+        day = format_date(day, "hollywood")
+        date_array.append(day)
+
 
     # select link and add to array 
     links = soup.find_all('a', class_="btn performance-buy-btn")
@@ -502,37 +479,115 @@ def hollywood(data):
 
     # iterate through arrays and add to json object 
     i = 0
-    while(i < len(headliner_array)):
-        data.append("Venue Website", "venue name", headliner_array[i], "date_array[i]", link_array[i])
+    while(i < len(link_array)):
+        data.append("Venue Website", "Hollywood Bowl Hollywood", headliner_array[i], date_array[i], link_array[i])
         i+=1
 
-    # close the connection 
-    driver.close() 
     return
    
 
 # Scrape Honda Center ###############################################################################################################
-def honda(data):
+def honda(driver, data):
+    driver.get('https://www.hondacenter.com/events/') 
+
+    # get the source html 
+    html = driver.page_source
+    # render all JS and stor as static html
+    soup = BeautifulSoup(html, "html.parser")
+
+    # create arrays to fill with data 
+    venue_array = []
+    headliner_array = []
+    date_array = []
+    link_array = []
+
+    # select headliner and add to array 
+    headliners = soup.find_all('h2', class_='event-list-name')
+
+    for headliner in headliners:
+        headliner_array.append(str(headliner.text).strip())
+
+    # select date and add to array 
+    dates = soup.find_all('div', class_="event-list-time")
+
+    for date in dates:
+        date = format_date(str(date.text).strip(), "hondacenter")
+        date_array.append(date)
+    
+    # select link and add to array 
+    links = soup.find_all('a', class_="button-round event-list-ticket-link")
+
+    for link in links:
+        link_array.append(str(link['href']).strip())
+        
+    # iterate through arrays and add to json object 
+    i = 0
+    while(i < len(headliner_array)):
+        data.append("Venue Website", "Honda Center", headliner_array[i], date_array[i], link_array[i])
+        i+=1
+
     return
+   
 
 # Scrape Microsoft Theater ##########################################################################################################
-def microsoft(data):
+def microsoft(driver, data):
+    driver.get('https://www.microsofttheater.com/events/all') 
+
+    # get the source html 
+    html = driver.page_source
+    # render all JS and stor as static html
+    soup = BeautifulSoup(html, "html.parser")
+
+    # create arrays to fill with data 
+    venue_array = []
+    headliner_array = []
+    date_array = []
+    link_array = []
+
+    # select headliner and add to array 
+    headliners = soup.find_all('h3', class_='title')
+
+    for headliner in headliners:
+        name = headliner.find('a')
+        headliner_array.append(name.text)
+
+    # select date and add to array 
+    dates = soup.find_all('div', class_="info clearfix")
+
+    for date in dates:
+        time = date.find('div', class_='date presented-by')
+        date = time['aria-label']
+        print(date)
+        date_array.append(date)
+    
+    # # select link and add to array 
+    # links = soup.find_all('a', class_="button-round event-list-ticket-link")
+
+    # for link in links:
+    #     link_array.append(str(link['href']).strip())
+        
+    # iterate through arrays and add to json object 
+    i = 0
+    while(i < len(headliner_array)):
+        data.append("Venue Website", "Microsoft Theater", headliner_array[i], date_array[i], "link_array[i]")
+        i+=1
+
     return
 
 # Scrape The Novo by Microsoft ######################################################################################################
-def novo(data):
+def novo(driver, data):
     return 
 
 # Scrape Shrine Auditorium ##########################################################################################################
-def shrine(data):
+def shrine(driver, data):
     return 
 
 # Scrape STAPLES Center #############################################################################################################
-def staples(data):
+def staples(driver, data):
     return 
 
 # Scrape Grammy Museum ##############################################################################################################
-def grammy(data):
+def grammy(driver, data):
     return 
 
 #####################################################################################################################################
@@ -545,23 +600,33 @@ def main():
     global data
     data = JsonInstance([])
 
+    global driver
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
     
     # collect data from each website 
-    # SB(data)
-    # fonda(data)
-    # forum(data)
-    # greekLA(data)
-    # greekBerkley(data)
-    hollywood(data)
-    # honda(data)
-    # microsoft(data)
-    # novo(data)
-    # shrine(data)
-    # staples(data)
-    # grammy(data)
+    # SB(driver, data)
+    # fonda(driver, data)
+    # forum(driver, data)
+    # greekLA(driver, data)
+    # greekBerkley(driver, data)
+    # hollywood(driver, data)
+    # honda(driver, data)
+    microsoft(driver, data)
+    # novo(driver, data)
+    # shrine(driver, data)
+    # staples(driver, data)
+    # grammy(driver, data)
 
     # output to json file 
     data.output("venueSitesData.json")
+    # close the connection 
+    driver.close() 
 
 if __name__ == "__main__":
     main()
